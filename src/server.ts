4b3e972762f69e639a0db31349734319f63316ec
Davi Dias <davifernandosousadias18@gmail.com>
2025-01-27 09:02:26 -0300

    fea(cors): configuring cors to be open to any route, * vulnerability*

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,25 @@
-import express from 'express'
-import { AppDataSource } from './database/data-source'
-import routes from './routes'
+import express from 'express';
+import cors from 'cors';
+import { AppDataSource } from './data-source';
+import routes from './routes';
 
 AppDataSource.initialize().then(() => {
-  const app = express()
+  const app = express();
 
-  app.use(express.json())
+  const corsOptions = {
+    origin: '*',  
+    methods: 'GET,POST,PUT,DELETE',  
+    allowedHeaders: 'Content-Type, Authorization', 
+  };
 
-  app.use(routes)
+  app.use(cors(corsOptions));  
+  app.use(express.json());
+  app.use(routes);
 
-  const port = process.env.API_PORT 
+  const port = process.env.API_PORT;
   app.listen(port, () => {
-    console.log(`Server is running on port ${port}`)
-  })
+    console.log(`Server is running on port ${port}`);
+  });
 }).catch(error => {
-  console.error("Error during DataSource initialization", error)
-})
+  console.error("Error during DataSource initialization", error);
+});
