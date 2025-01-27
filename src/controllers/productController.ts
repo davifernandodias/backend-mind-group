1b0ded76bc2b6884d2230e207dd894e92593ece9
Davi Dias <davifernandosousadias18@gmail.com>
2025-01-27 09:03:08 -0300

    fix(controller): added authenticated id in product controller

--- src/controllers/productController.ts
+++ src/controllers/productController.ts
@@ -5,25 +5,26 @@ import { userRepository } from "../repository/userRepository"
 export class ProductController {
 
   async create(req: Request, res: Response) {
-    const { name, description, price, image } = req.body
-    const { userId } = req.params
-
-    const user = await userRepository.findOneBy({ id: Number(userId) })
+    const { name, description, price, image } = req.body;
+    const userId = req.user.id; // Pega o ID do usuário autenticado
+  
+    const user = await userRepository.findOneBy({ id: userId });
     if (!user) {
-      return res.status(404).json({ message: "Usuário não encontrado" })
+      return res.status(404).json({ message: "Usuário não encontrado" });
     }
-
+  
     const newProduct = productRepository.create({
       name,
       description,
       price,
       image: image || null,
-      user: user
-    })
-
-    await productRepository.save(newProduct)
-    return res.status(201).json(newProduct)
+      user: user,
+    });
+  
+    await productRepository.save(newProduct);
+    return res.status(201).json(newProduct);
   }
+  
 
   async getAll(req: Request, res: Response) {
     const products = await productRepository.find()
