33bbba56fce4f838ac484751152fad20ed430c48
Davi Dias <davifernandosousadias18@gmail.com>
2025-01-26 21:19:53 -0300

    feat(controller): add method of updating product by id

--- src/controllers/productControllers.ts
+++ src/controllers/productControllers.ts
@@ -39,4 +39,20 @@ export class ProductController {
     return res.json(product)
   }
 
+  async update(req: Request, res: Response) {
+    const { id } = req.params
+    const { name, description, price, image } = req.body
+
+    let product = await productRepository.findOneBy({ id: Number(id) })
+    if (!product) {
+      return res.status(404).json({ message: "Produto não encontrado" })
+    }
+
+    product = { ...product, name, description, price, image: image || product.image }
+    await productRepository.save(product)
+
+    return res.json(product)
+  }
+
+
 }
